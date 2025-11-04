c9fe6d26342ac0d9330e89f8f75f80336ec11f74
ernesto571 <emmanueladesemoye@gmail.com>
2025-11-04 22:26:49 +0100

    fourth commit

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = "https://dailypulse-f8ra.onrender.com" || "http://localhost:5001";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
